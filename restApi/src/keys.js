73194b3df68cab50d3fef3be89d6d2c0cb7dbafb
Eduardo Robles <lozanoeduardo8777@gmail.com>
2023-11-30 10:46:16 -0600

    Change the keys of database

--- restApi/src/keys.js
+++ restApi/src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database:{
         host: 'sql3.freesqldatabase.com',
         user: 'sql3666501',
-        password: 'awul6dhtM8',
+        password: 'awuI6dhtM8',
         database: 'sql3666501'
     }
 } 
